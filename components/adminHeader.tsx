333e9c44cd6874c57ea11dda6cc1df201606e9ef
Craig McLean <craig.mclean39@gmail.com>
2022-02-04 15:03:22 -0800

    Add form to POST an rss feed to add a new podcast

--- components/adminHeader.tsx
+++ components/adminHeader.tsx
@@ -1,7 +1,7 @@
 import { Header, HeaderProps } from '@mantine/core';
 
 function AdminHeader(props: Omit<HeaderProps, 'children'>) {
-  return <Header {...props}>Custom header</Header>;
+  return <Header {...props}>True Crime Podcast Database</Header>;
 }
 
 export default AdminHeader;
--- pages/admin/addpodcast.tsx
+++ pages/admin/addpodcast.tsx
@@ -2,16 +2,30 @@ import type { NextPage } from 'next';
 import { useForm } from '@mantine/hooks';
 import AdminPage from '../../components/adminPage';
 import { Button, TextInput } from '@mantine/core';
+import axios from 'axios';
+import { useState } from 'react';
+import Router from 'next/router';
 
 const AddPodcast: NextPage = ({ children }) => {
+  const [serverError, setServerError] = useState<string>('');
   const form = useForm({
     initialValues: {
       rss: '',
     },
   });
 
-  const handleSubmit = (values: typeof form['values']) => {
-    console.log('AA');
+  const handleSubmit = async (values: typeof form['values']) => {
+    console.log(values);
+    try {
+      await axios.post('http://localhost:3001/api/podcast/create', values);
+      //SUCCESS
+      Router.push('/admin/podcastlist');
+    } catch (err) {
+      console.log('CATCH');
+      console.dir(err);
+      setServerError(err.response.data.message);
+      form.setFieldError('rss', true);
+    }
   };
 
   return (
@@ -22,6 +36,11 @@ const AddPodcast: NextPage = ({ children }) => {
           label='RSS Feed'
           placeholder='http://...'
           value={form.values.rss}
+          error={form.errors.rss && serverError}
+          onFocus={() => {
+            setServerError('');
+            form.setFieldError('rss', false);
+          }}
           onChange={(event) =>
             form.setFieldValue('rss', event.currentTarget.value)
           }
