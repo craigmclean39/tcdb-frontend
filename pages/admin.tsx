d5c8e90bae72c3fec5bf7cabbbf42c1231f4b91f
Craig McLean <craig.mclean39@gmail.com>
2022-02-13 00:05:00 -0800

    Remove admin specific components

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,8 +1,72 @@
 import type { NextPage } from 'next';
-import AdminLayout from '../components/adminLayout';
+import { useForm } from '@mantine/hooks';
+import { Button, TextInput, Loader, Divider } from '@mantine/core';
+import axios from 'axios';
+import { useState } from 'react';
+import Router from 'next/router';
+import Layout from '../components/layout';
 
 const Admin: NextPage = ({ children }) => {
-  return <AdminLayout>Root</AdminLayout>;
+  const [populating, setPopulating] = useState(false);
+  const [serverError, setServerError] = useState<string>('');
+  const [adding, setAdding] = useState(false);
+  const form = useForm({
+    initialValues: {
+      rss: '',
+    },
+  });
+
+  const handleSubmit = async (values: typeof form['values']) => {
+    try {
+      setAdding(true);
+      await axios.post('http://localhost:3001/api/podcast/create', values);
+      //SUCCESS
+      Router.push('/admin/podcasts');
+    } catch (err) {
+      setAdding(false);
+      setServerError('Error');
+      form.setFieldError('rss', true);
+    }
+  };
+
+  const handleClick = async () => {
+    setPopulating(true);
+    await axios.post('http://localhost:3001/api/populate');
+    setPopulating(false);
+  };
+
+  return (
+    <Layout>
+      <form onSubmit={form.onSubmit(handleSubmit)}>
+        <TextInput
+          required
+          label='RSS Feed'
+          placeholder='http://...'
+          value={form.values.rss}
+          error={form.errors.rss && serverError}
+          onFocus={() => {
+            setServerError('');
+            form.setFieldError('rss', false);
+          }}
+          onChange={(event) =>
+            form.setFieldValue('rss', event.currentTarget.value)
+          }
+        />
+
+        {!adding ? (
+          <Button my='lg' type='submit'>
+            Submit
+          </Button>
+        ) : (
+          <Loader></Loader>
+        )}
+      </form>
+      <Divider />
+      <Button onClick={handleClick} my='lg' loading={populating}>
+        Populate
+      </Button>
+    </Layout>
+  );
 };
 
 export default Admin;
